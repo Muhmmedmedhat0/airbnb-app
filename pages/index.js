c2992c478678a3e89eedff163a6d3a3c6afb6e83
MennaAllah Ahmed <78885127+mennallahmed@users.noreply.github.com>
2022-07-31 19:50:28 +0200

    files

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Footer from "../components/shared/footer/Footer";
 import Counter from '../components/counter/counter';
 import Theme from '../components/Theme/Theme';
 
@@ -7,6 +8,7 @@ function Home() {
       <Theme />
       <h1>Home</h1>
       <Counter />
+      <Footer/>
     </>
   );
 }
