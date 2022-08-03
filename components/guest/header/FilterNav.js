a53fc7a0941d66589b7b9fa0633999645738009a
monaamustafa <monaamustafa7@gmail.com>
2022-08-03 16:39:58 +0200

    filter nav v3

--- components/guest/header/FilterNav.js
+++ components/guest/header/FilterNav.js
@@ -3,12 +3,13 @@ import style from "../../../styles/filterNav.module.scss";
 import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
 import Image from "next/image";
+import{AiOutlineFilter} from 'react-icons/ai'
 
 function FilterNav() {
   const responsive = {
-    0: { items: 1 },
-    568: { items: 2 },
-    1024: { items: 10 },
+    0: { items: 2 },
+    568: { items: 5 },
+    1024: { items: 7 },
   };
   const myLoader = ({ src, width, quality }) => {
     return `https://a0.muscache.com/pictures/${src}`;
@@ -23,9 +24,7 @@ function FilterNav() {
         height={30}
         alt="fkjjf"
       />
-      <p>
-Islands
-</p>
+      <p>Islands</p>
     </div>,
     <div key={1} className="item" data-value="2">
       <Image
@@ -45,104 +44,107 @@ Islands
         height={30}
         alt="fkjjf"
       />
-      <p>
-
-National parks
-
-</p>
+      <p>National parks</p>
     </div>,
     <div key={3} className="item" data-value="4">
-    <Image
+      <Image
         loader={myLoader}
         src="3fb523a0-b622-4368-8142-b5e03df7549b.jpg"
         width={30}
         height={30}
         alt="fkjjf"
       />
-      <p>
-Amazing pools</p>
+      <p>Amazing pools</p>
     </div>,
     <div key={4} className="item" data-value="5">
-    <Image
+      <Image
         loader={myLoader}
         src="c5a4f6fc-c92c-4ae8-87dd-57f1ff1b89a6.jpg"
         width={30}
         height={30}
         alt="fkjjf"
       />
+      <p>OMG</p>
     </div>,
     <div key={3} className="item" data-value="4">
-    <Image
+      <Image
         loader={myLoader}
         src="35919456-df89-4024-ad50-5fcb7a472df9.jpg"
         width={30}
         height={30}
         alt="fkjjf"
       />
+      <p>Tiny homes</p>
     </div>,
     <div key={4} className="item" data-value="5">
-    <Image
+      <Image
         loader={myLoader}
         src="8b44f770-7156-4c7b-b4d3-d92549c8652f.jpg"
         width={30}
         height={30}
         alt="fkjjf"
       />
+      <p>Arctic</p>
     </div>,
     <div key={4} className="item" data-value="5">
-    <Image
+      <Image
         loader={myLoader}
         src="50861fca-582c-4bcc-89d3-857fb7ca6528.jpg"
         width={30}
         height={30}
         alt="fkjjf"
       />
+      <p>Design</p>
     </div>,
     <div key={3} className="item" data-value="4">
-    <Image
+      <Image
         loader={myLoader}
         src="52c8d856-33d0-445a-a040-a162374de100.jpg"
         width={30}
         height={30}
         alt="fkjjf"
       />
+      <p>Shared homes</p>
     </div>,
     <div key={4} className="item" data-value="5">
-    <Image
+      <Image
         loader={myLoader}
-        src="3fb523a0-b622-4368-8142-b5e03df7549b.jpg"
+        src="4221e293-4770-4ea8-a4fa-9972158d4004.jpg"
         width={30}
         height={30}
         alt="fkjjf"
       />
+      <p>Caves</p>
     </div>,
     <div key={4} className="item" data-value="5">
-    <Image
+      <Image
         loader={myLoader}
-        src="3fb523a0-b622-4368-8142-b5e03df7549b.jpg"
+        src="957f8022-dfd7-426c-99fd-77ed792f6d7a.jpg"
         width={30}
         height={30}
         alt="fkjjf"
       />
-    </div>
+      <p>Surfing</p>
+    </div>,
   ];
-  const handleDragStart = (e) => e.preventDefault();
-
   return (
-  
     <div className="container ">
       <div className={`d-flex ${style.filterBox}`}>
+        <div className={style.filterIconsBar}>
+        <AliceCarousel 
+          mouseTracking
+          items={items}
+          responsive={responsive}
+          disableDotsControls={true}
+          controlsStrategy="alternate"
+        />
         
-      <AliceCarousel
-        mouseTracking
-        items={items}
-        responsive={responsive}
-        disableDotsControls={true}
-        controlsStrategy="alternate"
-      />
-      <p >filter</p>
+        </div>
+        <div>
+          <p>filter</p>
+          <AiOutlineFilter/>
+        </div>
       </div>
-      
     </div>
   );
 }
